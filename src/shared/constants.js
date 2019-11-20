00063392242316b73b4a3fc0d0f119340ef40900
Gautham Sajith <gsajith@umich.edu>
2019-11-19 23:33:35 -0500

    Update constant

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -6,6 +6,6 @@ export const NO_AUTH = 'NO_AUTH';
 export const FETCH_INACTIVE = 'FETCH_INACTIVE';
 export const FETCH_IN_PROGRESS = 'FETCH_IN_PROGRESS';
 
-export const MILLIS_PER_EMAIL = 7;
+export const MILLIS_PER_EMAIL = 5;
 export const MAX_RESULTS = 200;
 export const PAGE_LIMIT = 2;
